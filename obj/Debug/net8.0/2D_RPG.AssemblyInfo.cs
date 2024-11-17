ea79a3de0e1ba9f5abda008cf0c5d106657527d8
NikolaosTsoukalis <ntsoukalis1@gmail.com>
2024-11-17 21:07:24 +0200

    pull changes from main

--- obj/Debug/net8.0/2D_RPG.AssemblyInfo.cs
+++ obj/Debug/net8.0/2D_RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2D_RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4fb3aa52cbebef4afc31ae5c5faee9340ca64f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba6e36814856640d38d33a97764600503b22af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("2D_RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2D_RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
