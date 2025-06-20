b05b619c7b6b7ec0a75eeaff8a26ffa372451b36
NikolaosTsoukalis <ntsoukalis1@gmail.com>
2025-06-20 16:37:31 +0300

    Slider build

---
+++ Engine/UI/Component/Slider.cs
@@ -0,0 +1,105 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _2D_RPG;
+
+public class Slider : ComponentBase
+{
+    public int CurrentValue;
+    private Vector2 CurrentThumbPosition;
+
+    public int Scale;
+
+    public Slider(GlobalEnumarations.ComponentType sliderType, int currentValue) : base(sliderType)
+    {
+        this.CurrentValue = currentValue;
+        this.CurrentThumbPosition = SetThumbPosition(CurrentValue);
+        if (CurrentThumbPosition == Vector2.Zero)
+        {
+            Console.WriteLine("ERROR on : '" + Type.ToString() + "', Position could not be set!");
+        }
+    }
+
+    private Vector2 SetThumbPosition(int variableValue)
+    {
+        Vector2 thumpPosition;
+        switch (this.Type)
+        {
+            case GlobalEnumarations.ComponentType.SliderHorizontal:
+                thumpPosition.X = this.Position.X + variableValue * Scale;
+                thumpPosition.Y = this.Position.Y;
+                break;
+            case GlobalEnumarations.ComponentType.SliderVertical:
+                thumpPosition.Y = this.Position.Y - variableValue * Scale;
+                thumpPosition.X = this.Position.X;
+                break;
+            default:
+                thumpPosition = Vector2.Zero;
+                break;
+        }
+
+        return thumpPosition;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        HandleStateChange();
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        var colour = Color.White;
+
+        Globals.SpriteBatch.Draw(base.TextureHandler.CurrentTexture, base.Bounds, colour);
+        Globals.SpriteBatch.Draw(base.TextureHandler.)
+    }
+
+    public override void HandleStateChange()
+    {
+        if (base.State == GlobalEnumarations.ComponentState.Free)
+        {
+            if (Globals.CurrentMouse.LeftButton == ButtonState.Pressed && Globals.PreviousMouse.LeftButton == ButtonState.Pressed)
+            {
+                HandleValueChange();
+            }   
+        }
+    }
+
+    public bool HandleValueChange()
+    {
+        try
+        {
+            int whole;
+            if (Globals.CurrentMouse.Position.X > Globals.PreviousMouse.Position.X)
+            {
+                if (Globals.CurrentMouse.LeftButton == ButtonState.Released && Globals.PreviousMouse.LeftButton == ButtonState.Released)
+                {
+                    switch (base.Type)
+                    {
+                        case GlobalEnumarations.ComponentType.SliderHorizontal:
+                            whole = (int)this.Position.X + this.TextureHandler.CurrentTexture.Width;
+                            CurrentValue = Globals.CurrentMouse.Position.X * 100 / whole;
+                            SetThumbPosition(CurrentValue);
+                            base.FunctionHandler.CallFunction();
+                            return true;
+                        case GlobalEnumarations.ComponentType.SliderVertical:
+                            whole = (int)this.Position.Y + this.TextureHandler.CurrentTexture.Height;
+                            CurrentValue = Globals.CurrentMouse.Position.Y * 100 / whole;
+                            SetThumbPosition(CurrentValue);
+                            base.FunctionHandler.CallFunction();
+                            return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("ERROR : " + e);
+            return false;
+        }
+    }
+}
